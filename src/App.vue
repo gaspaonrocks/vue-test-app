313f2d8571b15dfe01c670d78959d2760b80bbde
Sacha GRASSO <s.grasso@gop.link>
2017-09-15 11:26:05 +0200

    first real commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <navbar>
+    </navbar>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import navbar from './components/navbar/Navbar'
+
+Vue.component('navbar', navbar)
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import '../node_modules/clarity-icons/clarity-icons.min.css';
+@import '../node_modules/clarity-ui/clarity-ui.min.css';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main-container">
+    <header class="header header-6">
+      <div class="branding">
+        <img class="header-logo-resizer" src="../../assets/logo.png">
+        <span class="title">Vue-Test-App</span>
+      </div>
+      <div class="header-nav">
+        <a href="javascript://" class="active nav-link nav-text">Dashboard</a>
+        <a href="javascript://" class="nav-link nav-text">Interactive Analytics</a>
+      </div>
+      <div class="header-actions">
+        <a href="javascript://" class="nav-link nav-icon">
+          <clr-icon shape="cog"></clr-icon>
+        </a>
+      </div>
+    </header>
+    <nav class="subnav">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Management</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cloud</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tenants</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="content-container">
+      <div class="content-area">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src='./Navbar.style.css'></style>
+
+<script src='./Navbar.script.js'></script>
