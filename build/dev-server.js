6e8a2b7272aba7e97ef55ae8883bd88991ef711b
Sacha GRASSO <s.grasso@gop.link>
2017-09-19 17:53:40 +0200

    test sequelize

--- build/dev-server.js
+++ build/dev-server.js
@@ -75,6 +75,12 @@ var uri = 'http://localhost:' + port
 var _resolve
 var readyPromise = new Promise(resolve => {
   _resolve = resolve
+  var db = require('../server/models')
+  db.authenticate().then(() => {
+    console.log('connection success')
+  }).catch((e) => {
+    console.error('not so fast', e)
+  })
 })
 
 console.log('> Starting dev server...')
@@ -88,6 +94,8 @@ devMiddleware.waitUntilValid(() => {
 })
 
 var server = app.listen(port)
+
+
 
 module.exports = {
   ready: readyPromise,
